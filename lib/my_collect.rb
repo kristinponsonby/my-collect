948b62891d121ce66a0d457bcba89cfc779041c4
Kristin <kristinponsonby@gmail.com>
2021-02-13 16:31:54 -0500

    commit tree f5f4fd335688e17b7615c68372d17eaac59cda85

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+    i = 0
+    collect_array = []
+    while i < array.length
+      collect_array << yield(array[i])
+      i+=1
+    end
+    collect_array
+  end
 
 
+
+
+
+
+# Check list
+#{} can handle any empty array
+#{} yields corect element from an array
+#{} returns new array of capitalized languages
